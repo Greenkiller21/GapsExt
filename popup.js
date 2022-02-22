c5cd895b18f6178ab36cb852f4b713c947efbdd7
_Greenkiller_ <ericperonetti00@gmail.com>
2022-02-23 00:52:40 +0100

    Fixed spacing

--- popup.js
+++ popup.js
@@ -228,9 +228,9 @@ async function setAsSeen(course, grade) {
 
 function unicodeToChar(text) {
   return text.replace(/\\u[\dA-F]{4}/gi, 
-         function (match) {
-              return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
-         });
+  function (match) {
+    return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+  });
 }
 
 ///
@@ -246,14 +246,14 @@ function deepCompare() {
     // remember that NaN === NaN returns false
     // and isNaN(undefined) returns true
     if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
-         return true;
+      return true;
     }
 
     // Compare primitives and functions.     
     // Check if both arguments link to the same object.
     // Especially useful on the step where we compare prototypes
     if (x === y) {
-        return true;
+      return true;
     }
 
     // Works in case when functions are created in constructor.
@@ -264,71 +264,70 @@ function deepCompare() {
        (x instanceof RegExp && y instanceof RegExp) ||
        (x instanceof String && y instanceof String) ||
        (x instanceof Number && y instanceof Number)) {
-        return x.toString() === y.toString();
+      return x.toString() === y.toString();
     }
 
     // At last checking prototypes as good as we can
     if (!(x instanceof Object && y instanceof Object)) {
-        return false;
+      return false;
     }
 
     if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
-        return false;
+      return false;
     }
 
     if (x.constructor !== y.constructor) {
-        return false;
+      return false;
     }
 
     if (x.prototype !== y.prototype) {
-        return false;
+      return false;
     }
 
     // Check for infinitive linking loops
     if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
-         return false;
+      return false;
     }
 
     // Quick checking of one object being a subset of another.
     // todo: cache the structure of arguments[0] for performance
     for (p in y) {
-        if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
-            return false;
-        }
-        else if (typeof y[p] !== typeof x[p]) {
-            return false;
-        }
+      if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+        return false;
+      }
+      else if (typeof y[p] !== typeof x[p]) {
+        return false;
+      }
     }
 
     for (p in x) {
-        if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+      if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+        return false;
+      }
+      else if (typeof y[p] !== typeof x[p]) {
+        return false;
+      }
+
+      switch (typeof (x[p])) {
+        case 'object':
+        case 'function':
+          leftChain.push(x);
+          rightChain.push(y);
+
+          if (!compare2Objects (x[p], y[p])) {
             return false;
-        }
-        else if (typeof y[p] !== typeof x[p]) {
+          }
+
+          leftChain.pop();
+          rightChain.pop();
+          break;
+
+        default:
+          if (x[p] !== y[p]) {
             return false;
-        }
-
-        switch (typeof (x[p])) {
-            case 'object':
-            case 'function':
-
-                leftChain.push(x);
-                rightChain.push(y);
-
-                if (!compare2Objects (x[p], y[p])) {
-                    return false;
-                }
-
-                leftChain.pop();
-                rightChain.pop();
-                break;
-
-            default:
-                if (x[p] !== y[p]) {
-                    return false;
-                }
-                break;
-        }
+          }
+          break;
+      }
     }
 
     return true;
@@ -340,13 +339,12 @@ function deepCompare() {
   }
 
   for (i = 1, l = arguments.length; i < l; i++) {
+    leftChain = []; //Todo: this can be cached
+    rightChain = [];
 
-      leftChain = []; //Todo: this can be cached
-      rightChain = [];
-
-      if (!compare2Objects(arguments[0], arguments[i])) {
-          return false;
-      }
+    if (!compare2Objects(arguments[0], arguments[i])) {
+      return false;
+    }
   }
 
   return true;
